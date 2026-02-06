284d536daa9dce23a2f96af9c9cb21957f37916f
Janvi <jahnavibolisetti0508@gmail.com>
2026-02-06 10:33:07 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Heyy EveryONE!!</h1>
+
+    <h2>Hello People</h2>
+    <ul>
+      <li>harry</li>
+      <li>dulquer</li>
+    </ul>
+    
+      
+    </>
+  )
+}
+
+export default App
